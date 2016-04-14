954a8b05581b9e8b1568514b0c2e0b25065aa25f
nisiyu <nisiyu91@gmail.com>
2016-04-15 01:01:13 +0800

    add dist

---
+++ dist/js/present.js
@@ -0,0 +1,1 @@
+var gift={popup:function(){$("div.mask").css({display:"block"}),$("div.present_dialog").css({display:"block"})},close:function(){$("div.mask").css({display:"none"}),$("div.present_dialog").css({display:"none"})}};
---
+++ dist/js/remadjust.js
@@ -0,0 +1,1 @@
+!function(e){function t(){var e=o.getBoundingClientRect().width;e>640&&(e=640);var t=e/6.4;o.style.fontSize=t+"px"}var n,i=e.document,o=i.documentElement;e.addEventListener("resize",function(){clearTimeout(n),n=setTimeout(t,300)},!1),e.addEventListener("pageshow",function(e){e.persisted&&(clearTimeout(n),n=setTimeout(t,300))},!1),t()}(window);
